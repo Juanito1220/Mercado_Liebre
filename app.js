cadf5f346adcb5c7e2eab0bc73769d8d4bc179d6
Juan Pablo <proyectosoftwarejp@gmail.com>
2022-02-04 12:33:56 -0500

    Parte 1

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path= require("path");
+app.use(express.static('Public'));
+
+
+app.listen(3030, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/views/home.html'));
+});
